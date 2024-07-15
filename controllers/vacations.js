530741a1e91f70538a015f4ba10c5ed5597ca886
MathiasHun1 <141583942+MathiasHun1@users.noreply.github.com>
2024-07-15 13:50:25 +0200

    added more routes to vacations collection

--- controllers/vacations.js
+++ controllers/vacations.js
@@ -1,15 +1,26 @@
 const vacationsRouter = require('express').Router()
+const { response } = require('express')
 const Vacation = require('../models/vacation')
+
+// get vacations data
+vacationsRouter.get('/', (request, response) => {
+  Vacation
+    .find({})
+    .then(result => {
+      response.json(result)
+    })
+})
 
 // ADD new vacations obj
 vacationsRouter.post('/', (request, response) => {
   const {onVacation, text} = request.body
 
-  if(!onVacation, !text) {
+  if(!onVacation || !text) {
     return response.status(400).json({error: 'Content missing!'})
   }
 
   const vacationObj = new Vacation({ onVacation, text })
+
   vacationObj
     .save()
     .then(result => {
@@ -18,6 +29,25 @@ vacationsRouter.post('/', (request, response) => {
     .catch(error => {
       response.status(400).json({error: error.message})
     })
+})
+
+vacationsRouter.put('/', (request, response) => {
+  const { onVacation, text } = request.body
+
+  if(onVacation === undefined || !text) {
+    return response.status(400).json({error: 'Content missing!'})
+  }
+
+  Vacation.findOne({})
+    .then(result => {
+      result.onVacation = onVacation
+      result.text = text
+      result
+        .save()
+        .then( r => response.status(200).end() )
+    })
+    .catch(error => response.json({error: error.message}))
+
 })
 
 module.exports = vacationsRouter
